383523e3cfa58efa857d296a99cd2919b503c88d
Bobbaru <bobbaru_krueger@yahoo.com>
2023-10-14 02:18:06 +0300

    Displaying platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,16 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGamesResponse {
@@ -22,7 +28,7 @@ const useGames = () => {
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
       .then(({ data }) => {
-        console.log(data.results);
+        // console.log(data.results);
         setGames(data.results);
       })
       .catch((error) => {
