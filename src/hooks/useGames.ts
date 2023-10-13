e556ec000bab415e19a955b1fdbb980a66e3c0fb
Bobbaru <bobbaru_krueger@yahoo.com>
2023-10-14 02:43:23 +0300

    Show loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -12,7 +12,7 @@ export interface Game {
   name: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
-  metacritic: number
+  metacritic: number;
 }
 
 interface FetchGamesResponse {
@@ -23,24 +23,31 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
+
+    setLoading(true);
+
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
       .then(({ data }) => {
         // console.log(data.results);
+
         setGames(data.results);
+        setLoading(false);
       })
       .catch((error) => {
         if (error instanceof CanceledError) return;
         setError(error.message);
+        setLoading(false);
       });
 
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
